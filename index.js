41ff7958c9fcadefe0c978a40e56937a6af758e5
Tyler Bonnell <tbonnell@uw.edu>
2016-03-27 12:42:50 -0700

    bootstrap

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ var letters = "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM-_"
 
 window.onload = function() {
   $("input").onkeydown = shorten;
+  $("shorten").onmouseup = getFromServer;
 };
 
 function shorten(e) {
   if (e.keyCode != 13) return;
-
-  getFromServer($("input").value);
+  getFromServer();
 }
 
-function getFromServer(inURL) {
-  var url = "https://sphp-tylerbonnell.rhcloud.com/?url=" + encodeURIComponent(inURL);
+function getFromServer() {
+  var url = "http://sphp-tylerbonnell.rhcloud.com/?url=" + encodeURIComponent($("input").value);
   var ajax = new XMLHttpRequest();
   ajax.onload = function() {
     $("shortened").innerHTML = this.responseText;
