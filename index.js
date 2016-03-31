3c1686438c3f8ea0e76a4afe655defb1597c12a4
Tyler Bonnell <tbonnell@uw.edu>
2016-03-31 00:01:51 -0700

    cleaned up/added some comments

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
-var letters = "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM-_";
-
-window.onload = function() {
-  $("input").onkeydown = shorten;
-  $("shorten").onmouseup = getFromServer;
-};
-
-function shorten(e) {
-  if (e.keyCode != 13) return;
-  getFromServer();
-}
-
-function getFromServer() {
-  $("shortened").innerHTML = "";
-  var url = "http://sphp-tylerbonnell.rhcloud.com/?url=" + encodeURIComponent($("input").value);
-  var ajax = new XMLHttpRequest();
-  ajax.onload = function() {
-    $("shortened").style.display = "";
-    $("shortened").innerHTML = this.responseText;
+(function() {
+  window.onload = function() {
+    $("input").onkeydown = shorten;
+    $("shorten").onmouseup = getFromServer;
   };
-  ajax.open("GET", url, true);
-  ajax.send();
-}
 
-function $(el) {
-  return document.getElementById(el);
-}
+  // user can hit enter instead of clicking
+  function shorten(e) {
+    if (e.keyCode == 13) {
+      getFromServer();
+    }
+  }
+
+  // Query the php backend with the currently entered URL as a parameter
+  function getFromServer() {
+    $("shortened").innerHTML = "";
+    var url = "http://sphp-tylerbonnell.rhcloud.com/?url=" + encodeURIComponent($("input").value);
+    var ajax = new XMLHttpRequest();
+    ajax.onload = function() {
+      $("shortened").style.display = "";
+      $("shortened").innerHTML = this.responseText;
+    };
+    ajax.open("GET", url, true);
+    ajax.send();
+  }
+
+  function $(el) {
+    return document.getElementById(el);
+  }
+}());
