dbaf353aeeb5a521d698ef1d723b17a3785bd34f
Tyler Bonnell <tbonnell@uw.edu>
2016-04-02 19:17:56 -0700

    some comments/encapsulation

--- otherpages.js
+++ otherpages.js
@@ -1,16 +1,25 @@
-window.onload = function() {
-  var addr = window.location.href.substring(window.location.href.length - 3, window.location.href.length);
-  var url = "http://sphp-tylerbonnell.rhcloud.com/?redirect=" + addr;
-  var ajax = new XMLHttpRequest();
-  ajax.onload = function() {
-    txt = this.responseText;
-    console.log(txt);
-    if (txt == "invalid url") {
-      window.location.href = "http://bonn.pw/s/";
-    } else {
-      window.location.href = txt;
-    }
-  };
-  ajax.open("GET", url, true);
-  ajax.send();
-}
+/*
+  If a user loads a page that isn't explicitely defined,
+  the html for 404.html is loaded. This js will parse
+  the url and see if it is a valid redirect URL. If it
+  is, then it redirects. Otherwise, it goes to the homepage.
+*/
+
+(function() {
+  window.onload = function() {
+    var addr = window.location.href.substring(window.location.href.length - 3, window.location.href.length);
+    var url = "http://sphp-tylerbonnell.rhcloud.com/?redirect=" + addr;
+    var ajax = new XMLHttpRequest();
+    ajax.onload = function() {
+      txt = this.responseText;
+      console.log(txt);
+      if (txt == "invalid url") {
+        window.location.href = "http://bonn.pw/s/";
+      } else {
+        window.location.href = txt;
+      }
+    };
+    ajax.open("GET", url, true);
+    ajax.send();
+  }
+}());
